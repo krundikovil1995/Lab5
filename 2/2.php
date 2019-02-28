bd6ca9767c14b3f8e3997cfcb669516578bbdeef
krundikovil1995 <20krundikovil1995@mail.ru>
2019-02-28 15:31:48 +0300

    updated the 2 task of the 5 lab!

--- 2/2.php
+++ 2/2.php
@@ -12,17 +12,17 @@ $sql = "SHOW TABLES";
 $result=$mysqli->query($sql);
 
 if ($result->num_rows>0) {
-     echo "<br> Таблицы базы данных:";
+     echo "<br>  Таблицы базы данных:";
     while ($row = $result->fetch_assoc()) {
-        echo "<br><strong> " . $row["Tables_in_testdb"]."</strong>";
+        echo "<br><strong> " . $row["Tables_in_testdb"].":</strong>";
         $table = $row["Tables_in_testdb"];
         $sql = "DESCRIBE $table";
         $res = $mysqli->query($sql);
 
         if ($res->num_rows >0){
-            echo "<br> Данная таблица содержит:";
+            echo "<br> &nbsp&nbsp&nbspДанная таблица содержит: ";
             while ($row2 = $res->fetch_assoc()){
-                echo "<br> Field: ".$row2["Field"]."<br> Type: ".$row2["Type"]."<br> Null: ".$row2["Null"]."<br> key: ". $row2["Key"];
+                echo "<br>&nbsp&nbsp&nbsp&nbsp&nbsp - Field: ".$row2["Field"]."<br>&nbsp&nbsp&nbsp&nbsp&nbsp - Type: ".$row2["Type"]."<br>&nbsp&nbsp&nbsp&nbsp&nbsp - Null: ".$row2["Null"]."<br>&nbsp&nbsp&nbsp&nbsp&nbsp - key: ". $row2["Key"];
             }
         }
     }
